ba80b8fe57a78147f7b697dd24f413ca182a57cc
EstebanPechuan <esteban.pechuan@gmail.com>
2022-03-20 03:07:56 -0300

    Interacción tabla desplegables

--- app/Views/servicios/sistemas.php
+++ app/Views/servicios/sistemas.php
@@ -15,30 +15,52 @@
     </section>
 
 
+    <!-- let flecha = document.createElement('div')
+    flecha.setAttribute('class', 'flecha-ver-mas')
+
+    flecha.innerText = 'hola'
+
+    item.appendChild(flecha) -->
+
+
     <section class="depto-servicios">
         <div class="tabla-servicios">
             <div class="cada-servicio">
                 <div class="serv-nombre">Servicio / Aplicación</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema</div>
+                <div class="serv-descripcion">
+                    <div>Sistema</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
             
             <div class="cada-servicio">
                 <div class="serv-nombre">SIIF</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Gestión de procedimientos e información financiera</div>
+                <div class="serv-descripcion">
+                    <div>Gestión de procedimientos e información financiera</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">SIGED</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema de Gestión Documental (Seguimiento de expedientes)</div>
+                <div class="serv-descripcion">
+                    <div>Sistema de Gestión Documental (Seguimiento de expedientes)</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">SIPE</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Gestión de procedimientos e información financiera, y brinda información en línea sobre el presupuesto, la tesorería y los subsistemas contables, al mismo tiempo que facilita la generación oportuna de informes confiables sobre la ejecución presupuestaria.</div>
+                <div class="serv-descripcion">
+                    <div>Gestión de procedimientos e información financiera, y brinda información en línea sobre el presupuesto, la tesorería y los subsistemas contables, al mismo tiempo que facilita la generación oportuna de informes confiables sobre la ejecución presupuestaria.</div>
+                </div>
                 
                 <div class="flecha-ver-mas">v</div>
             </div>
@@ -46,37 +68,59 @@
             <div class="cada-servicio">
                 <div class="serv-nombre">SIARH</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema integrado de las personas jurídicas y el registro civil</div>
+                <div class="serv-descripcion">
+                    <div>Sistema integrado de las personas jurídicas y el registro civil</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">IPV</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema Integrado de Administración de Recursos Humanos</div>
+                <div class="serv-descripcion">
+                    <div>Sistema Integrado de Administración de Recursos Humanos</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">SIGOP</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema de Gestión Instituto Provincial de la Vivienda</div>
+                <div class="serv-descripcion">
+                    <div>Sistema de Gestión Instituto Provincial de la Vivienda</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">COMPRAS PÚBLICAS</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion"></div>
+                <div class="serv-descripcion">
+                    <div></div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">SUELDOS</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema de carga de novedades y liquidación de sueldos</div>
+                <div class="serv-descripcion">
+                    <div>Sistema de carga de novedades y liquidación de sueldos</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">SIIS</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema Integral Informático de Seguridad ( carga digital de sumarios; efectos denunciados, personas desaparecidas, formulario de violencia familiar, exposiciones, actuaciones preliminares, expedientes contravencionales, aprehensiones, depósito judicial)</div>
+                <div class="serv-descripcion">
+                    <div>Sistema Integral Informático de Seguridad ( carga digital de sumarios; efectos denunciados, personas desaparecidas, formulario de violencia familiar, exposiciones, actuaciones preliminares, expedientes contravencionales, aprehensiones, depósito judicial)</div>
+                </div>
                 
                 <div class="flecha-ver-mas">v</div>
             </div>
@@ -84,7 +128,9 @@
             <div class="cada-servicio">
                 <div class="serv-nombre">UNIDE</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Unidad de Infraestructura de Datos Espaciales. Garantiza la generación, el acceso y la distribución de datos e información georreferenciada confiable en todo el territorio provincial</div>
+                <div class="serv-descripcion">
+                    <div>Unidad de Infraestructura de Datos Espaciales. Garantiza la generación, el acceso y la distribución de datos e información georreferenciada confiable en todo el territorio provincial</div>
+                </div>
                 
                 <div class="flecha-ver-mas">v</div>
             </div>
@@ -92,19 +138,29 @@
             <div class="cada-servicio">
                 <div class="serv-nombre">SUSIP</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema Único de Seguimiento Integral de Personas</div>
+                <div class="serv-descripcion">
+                    <div>Sistema Único de Seguimiento Integral de Personas</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">RCDIGITAL</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Registro Civil</div>
+                <div class="serv-descripcion">
+                    <div>Registro Civil</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">RUNMEPI</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Registro Único Nominal de Medidas de Protección Integral. Permite contar con información actualizada y en tiempo real de las acciones que se desarrolla con cada niño y adolescente, favoreciendo la comunicación e intercambio entre los equipos, contribuyendo a mejorar las prácticas y agilizar los procedimientos a la vez que brinda datos estadísticos de valor para la construcción de la política pública.</div>
+                <div class="serv-descripcion">
+                    <div>Registro Único Nominal de Medidas de Protección Integral. Permite contar con información actualizada y en tiempo real de las acciones que se desarrolla con cada niño y adolescente, favoreciendo la comunicación e intercambio entre los equipos, contribuyendo a mejorar las prácticas y agilizar los procedimientos a la vez que brinda datos estadísticos de valor para la construcción de la política pública.</div>
+                </div>
                 
                 <div class="flecha-ver-mas">v</div>
             </div>
@@ -112,7 +168,9 @@
             <div class="cada-servicio">
                 <div class="serv-nombre">ANDES</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Plataforma digital orientada a pacientes y miembros de equipos de salud. Permite acceder y gestionar datos de salud de manera ágil y segura.</div>
+                <div class="serv-descripcion">
+                    <div>Plataforma digital orientada a pacientes y miembros de equipos de salud. Permite acceder y gestionar datos de salud de manera ágil y segura.</div>
+                </div>
                 
                 <div class="flecha-ver-mas">v</div>
             </div>
@@ -120,25 +178,39 @@
             <div class="cada-servicio">
                 <div class="serv-nombre">DIGESTO JURIDICO</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion"></div>
+                <div class="serv-descripcion">
+                    <div></div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">DIGESTO ADMIN.</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion"></div>
+                <div class="serv-descripcion">
+                    <div></div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">TURNERO</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema de Gestión Documental (Seguimiento de expedientes)</div>
+                <div class="serv-descripcion">
+                    <div>Sistema de Gestión Documental (Seguimiento de expedientes)</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">CIDI APP</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Aplicación móvil de autogestión que permite al ciudadano realizar de manera online, a través de una única cuenta personal, diversos trámites y servicios que brinda el Gobierno de la Provincia de San Juan</div>
+                <div class="serv-descripcion">
+                    <div>Aplicación móvil de autogestión que permite al ciudadano realizar de manera online, a través de una única cuenta personal, diversos trámites y servicios que brinda el Gobierno de la Provincia de San Juan</div>
+                </div>
                 
                 <div class="flecha-ver-mas">v</div>
             </div>
@@ -146,7 +218,9 @@
             <div class="cada-servicio">
                 <div class="serv-nombre">CIDI WEB</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sitio web de autogestión que permite al ciudadano realizar de manera online, a través de una única cuenta personal, diversos trámites y servicios que brinda el Gobierno de la Provincia de San Juan.</div>
+                <div class="serv-descripcion">
+                    <div>Sitio web de autogestión que permite al ciudadano realizar de manera online, a través de una única cuenta personal, diversos trámites y servicios que brinda el Gobierno de la Provincia de San Juan.</div>
+                </div>
                 
                 <div class="flecha-ver-mas">v</div>
             </div>
@@ -154,19 +228,31 @@
             <div class="cada-servicio">
                 <div class="serv-nombre">CONSUMAN</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema de gestión de activos físicos y mantenimiento</div>
+                <div class="serv-descripcion">
+                    <div>Sistema de gestión de activos físicos y mantenimiento</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">BI</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Sistema para inteligencia de negocios</div>
+                <div class="serv-descripcion">
+                    <div>Sistema para inteligencia de negocios</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
 
             <div class="cada-servicio">
                 <div class="serv-nombre">BPM</div>
                 <div class="linea-vert"></div>
-                <div class="serv-descripcion">Plataforma de Gestión de Procesos de Negocios (BPM)</div>
+                <div class="serv-descripcion">
+                    <div>Plataforma de Gestión de Procesos de Negocios (BPM)</div>
+                </div>
+                
+                <div class="flecha-ver-mas">v</div>
             </div>
         </div>
     </section>
--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,21 @@ const desplegables = document.querySelectorAll('.cada-servicio')
 
 desplegables.forEach(item => {
     item.addEventListener('click', () => {
-        console.log(item);
-        // console.log(item.children[4]);
         item.children[2].classList.toggle('serv-descripcion-click')
         item.children[3].classList.toggle('flecha-ver-mas-click')
     })
+
+    // Agregar o no flecha desplegable
+    let anchoSpan = item.children[2].children[0].clientWidth
+    let anchoPadre = item.children[2].clientWidth
+
+
+    // console.log(anchoSpan);
+    // console.log(anchoPadre);
+
+    // Diferencia entre contenido y su padre (110 es el padding total)
+    if (anchoSpan >= (anchoPadre - 110)) {
+        item.children[3].classList.add('active')
+        item.children[2].children[0].classList.add('display-inline')
+    }
 });
